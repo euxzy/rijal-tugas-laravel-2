c047e4155f211c0e6f3ac9ef5e5598794115fd92
euxzy <rijal.m12l@gmail.com>
2022-12-12 15:27:06 +0700

    update list product page

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -1,9 +1,11 @@
 @extends('templates.product')
 @section('title', 'List Product')
 @section('content')
-  <section class="w-[1360px] mx-auto mt-14 p-5 relative">
-    <a href="{{ route('product.add') }}" class="absolute top-6 right-10 inline-block mb-4 bg-green-600 text-white px-4 py-2 rounded-lg">Tambah Produk</a>
-    <h1 class="w-max mx-auto font-semibold text-4xl mb-10 text-gray-700">Daftar Produk</h1>
+  <section class="lg:max-w-[1360px] mx-auto mt-14 p-5 relative">
+    <div class="absolute top-20 left-0 w-full flex justify-center">
+      <a href="{{ route('product.add') }}" class="inline-block bg-green-600 text-white px-4 py-2 rounded-lg mx-auto">Tambah Produk</a>
+    </div>
+    <h1 class="w-max mx-auto font-semibold text-4xl mb-24 text-gray-700">Daftar Produk</h1>
     <div class="flex gap-16 justify-center flex-wrap">
       @foreach ($products as $product)
         @include('components.product-card', $product)
