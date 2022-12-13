3d8bf5f57d282dce8e9ccceaf9ee6b04f59c2dda
euxzy <rijal.m12l@gmail.com>
2022-12-13 21:01:32 +0700

    update post-card.blade.php

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,9 +1,9 @@
-<div class="flex items-center bg-white w-[47%] gap-5 p-3 rounded-xl">
-  <div class="w-72 h-48 rounded-xl overflow-hidden flex justify-center items-center">
-    <img src="{{ asset('images/posts/' . $article->image) }}" alt="{{ $article->title }}" class="w-full">
+<a href="{{ route('post.show', $article->slug) }}" class="group flex items-center bg-white w-[95%] lg:w-[47%] gap-5 p-3 mx-auto rounded-xl shadow-md hover:shadow-xl transition-all duration-300">
+  <div class="w-48 h-32 lg:w-72 lg:h-48 rounded-xl overflow-hidden flex justify-center items-center">
+    <img src="{{ asset('images/posts/' . $article->image) }}" alt="{{ $article->title }}" class="w-full group-hover:scale-125 transition-all duration-300">
   </div>
   <div class="flex-1">
-    <a href="{{ route('post.show', $article->slug) }}" class="inline-block font-medium text-xl mb-4">{{ $article->title }}</a>
+    <p class="inline-block font-medium text-xl mb-4">{{ $article->title }}</p>
     <p class="text-sm">{{ strip_tags(substr($article->content, 0, 100) . '...') }}</p>
   </div>
-</div>
+</a>
