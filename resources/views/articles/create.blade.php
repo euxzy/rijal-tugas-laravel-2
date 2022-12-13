0768388b4d7e957a1e37ab8ae1045dcaac5b3b86
euxzy <rijal.m12l@gmail.com>
2022-12-13 16:04:19 +0700

    styling edit post page

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -12,8 +12,8 @@
         <input type="text" name="title" id="title" placeholder="Judul Artikel" class="w-[47%] px-3 py-2 rounded-lg outline-none focus:outline-1 focus:outline-gray-400 transition-all duration-300" required>
         <input type="text" name="slug" id="slug" placeholder="Slug" class="w-[47%] px-3 py-2 rounded-lg outline-none focus:outline-1 focus:outline-gray-400 transition-all duration-300" required>
       </div>
-      <input type="hidden" name="content" id="content" class="hidden" required>
-      <input type="file" name="image" id="image" accept="image/*" class="w-full mb-8 bg-white cursor-pointer file:cursor-pointer rounded-lg file:hover:bg-green-500 file:bg-green-700 file:px-3 file:py-2 file:border-none file:text-green-100 file:mr-4 text-gray-500 font-medium file:transition-all file:duration-300">
+      <input type="hidden" name="content" id="content" class="hidden">
+      <input type="file" name="image" id="image" accept="image/*" class="w-full mb-8 bg-white cursor-pointer file:cursor-pointer rounded-lg file:hover:bg-green-500 file:bg-green-700 file:px-3 file:py-2 file:border-none file:text-green-100 file:mr-4 text-gray-500 font-medium file:transition-all file:duration-300" required>
       <trix-editor input="content" class="mb-8 w-full min-h-[300px] bg-white cursor-pointer file:cursor-pointer rounded-lg file:hover:bg-green-500 file:bg-green-700 file:px-3 file:py-2 file:border-none file:text-green-100 file:mr-4 text-gray-500 font-medium file:transition-all file:duration-300"></trix-editor>
       <button type="submit" class="px-3 py-2 rounded-lg text-white bg-green-600">Tambah Post</button>
     </form>
--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -1,16 +1,22 @@
 @extends('templates.base')
 @section('title', 'Edit Post')
 @section('content')
-  <section>
+@include('components.navbar')
+  <section class="container my-28">
+    <div class="w-full flex justify-center mb-12">
+      <h1 class="text-4xl font-bold text-gray-700">Edit Post</h1>
+    </div>
     <form action="{{ route('post.update', $article->id) }}" method="post" enctype="multipart/form-data">
       @method('put')
       @csrf
-      <input type="text" name="title" id="title" placeholder="Judul Artikel" value="{{ $article->title }}">
-      <input type="text" name="slug" id="slug" placeholder="Slug" value="{{ $article->slug }}">
-      <input type="file" name="image" id="image" accept="image/*" class="">
+      <div class="flex justify-between w-full mb-8">
+        <input type="text" name="title" id="title" placeholder="Judul Artikel" value="{{ $article->title }}" class="w-[47%] px-3 py-2 rounded-lg outline-none focus:outline-1 focus:outline-gray-400 transition-all duration-300" required>
+        <input type="text" name="slug" id="slug" placeholder="Slug" value="{{ $article->slug }}" class="w-[47%] px-3 py-2 rounded-lg outline-none focus:outline-1 focus:outline-gray-400 transition-all duration-300" required>
+      </div>
       <input type="hidden" name="content" id="content" class="hidden">
-      <trix-editor input="content">{!! $article->content !!}</trix-editor>
-      <button type="submit">Submit</button>
+      <input type="file" name="image" id="image" accept="image/*"  class="w-full mb-8 bg-white cursor-pointer file:cursor-pointer rounded-lg file:hover:bg-green-500 file:bg-green-700 file:px-3 file:py-2 file:border-none file:text-green-100 file:mr-4 text-gray-500 font-medium file:transition-all file:duration-300">
+      <trix-editor input="content"  class="mb-8 w-full min-h-[300px] bg-white cursor-pointer file:cursor-pointer rounded-lg file:hover:bg-green-500 file:bg-green-700 file:px-3 file:py-2 file:border-none file:text-green-100 file:mr-4 text-gray-500 font-medium file:transition-all file:duration-300">{!! $article->content !!}</trix-editor>
+      <button type="submit"  class="px-3 py-2 rounded-lg text-white bg-green-600">Simpan Perubahan</button>
     </form>
   </section>
 @endsection
