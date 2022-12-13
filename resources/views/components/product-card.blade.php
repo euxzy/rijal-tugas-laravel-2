73e9a2af0f78ff1949042353503ee85761159122
euxzy <rijal.m12l@gmail.com>
2022-12-13 21:20:34 +0700

    update views

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,9 +1,9 @@
-<div class="w-80 bg-white p-4 rounded-lg shadow-lg">
+<div class="group w-80 bg-white p-4 rounded-lg shadow-lg">
   <div class="w-full h-64 overflow-hidden rounded-lg flex items-center justify-center bg-slate-200 mb-4">
     @if (!file_exists(public_path('images/products/' . $product->image)))
-      <h1 class="text-center font-semibold text-5xl text-gray-700">{{ $product->name }}</h1>
+      <h1 class="text-center font-semibold text-5xl text-gray-700 group-hover:scale-125 transition-all duration-300">{{ $product->name }}</h1>
     @else
-      <img src="{{ asset('images/products/'. $product->image) }}" alt="{{ $product->name }}" class="w-full scale-150">
+      <img src="{{ asset('images/products/'. $product->image) }}" alt="{{ $product->name }}" class="w-full scale-150 group-hover:scale-[2] transition-all duration-300">
     @endif
   </div>
   <div class="flex justify-between items-center mb-3">
@@ -18,7 +18,7 @@
   </div>
   <p class="mb-4 text-gray-900">{{ $product->description }}</p>
   <div class="flex gap-4">
-    <a href="{{ route('product.edit', $product->id) }}" class="inline-block mb-4 bg-green-600 text-white px-4 py-2 rounded-lg">Edit</a>
-    <a href="{{ route('product.delete', $product->id) }}" class="inline-block mb-4 bg-red-600 text-white px-4 py-2 rounded-lg">Hapus</a>
+    <a href="{{ route('product.edit', $product->id) }}" class="inline-block mb-4 bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-opacity-80 hover:-translate-y-1 transition-all duration-300">Edit</a>
+    <a href="{{ route('product.delete', $product->id) }}" class="inline-block mb-4 bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-opacity-80 hover:-translate-y-1 transition-all duration-300">Hapus</a>
   </div>
 </div>
--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -4,7 +4,7 @@
 @include('components.navbar')
   <section class="container my-24 mx-auto p-5 relative">
     <div class="absolute top-20 left-0 w-full flex justify-center">
-      <a href="{{ route('product.add') }}" class="inline-block bg-green-600 text-white px-4 py-2 rounded-lg mx-auto">Tambah Produk</a>
+      <a href="{{ route('product.add') }}" class="inline-block bg-green-600 text-white px-4 py-2 rounded-lg mx-auto hover:bg-opacity-80 hover:-translate-y-1 transition-all duration-300">Tambah Produk</a>
     </div>
     <h1 class="w-max mx-auto font-semibold text-4xl mb-24 text-gray-700">Daftar Produk</h1>
     <div class="flex gap-14 justify-center flex-wrap">
