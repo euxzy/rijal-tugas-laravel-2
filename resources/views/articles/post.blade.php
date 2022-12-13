397d76a7fe4547e3238c5917f62e86a2c6971ba5
euxzy <rijal.m12l@gmail.com>
2022-12-13 15:11:09 +0700

    styling single post page

--- resources/views/articles/post.blade.php
+++ resources/views/articles/post.blade.php
@@ -1,9 +1,15 @@
 @extends('templates.base')
 @section('title', $article->title)
 @section('content')
-  <section>
-    <h1>{{ $article->title }}</h1>
-    <div><img src="{{ asset('images/posts/' . $article->image) }}" alt="{{ $article->title }}"></div>
-    <p>{!! $article->content !!}</p>
+@include('components.navbar')
+  <section class="container my-32">
+    <h1 class="text-4xl font-bold text-gray-700 mb-10">{{ $article->title }}</h1>
+    <div class="w-full h-96 overflow-hidden flex items-center justify-center mb-3">
+      <img src="{{ asset('images/posts/' . $article->image) }}" alt="{{ $article->title }}" class="w-full">
+    </div>
+    <p class="mb-8 w-full text-right">{{ $article->updated_at }}</p>
+    <p class="text-gray-900">{!! $article->content !!}</p>
+
+    <a href="{{ route('post.list') }}" class="inline-block mt-20 bg-green-600 text-white px-4 py-2 rounded-lg font-bold">Lihat post lain</a>
   </section>
 @endsection
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<section class="bg-white/50 backdrop-blur-sm absolute w-full shadow-md top-0 left-0">
+<section class="bg-white/50 backdrop-blur-sm fixed w-full shadow-md top-0 left-0">
   <nav class="flex container mx-auto justify-between h-20 items-center">
     <div class="flex gap-20 items-center">
       <a href="{{ route('home') }}" class="font-bold text-2xl">BrandName</a>
